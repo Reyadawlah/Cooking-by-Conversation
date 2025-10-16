3600009d668967ec55f79bc8e980edb910143611
Reya Dawlah <reya.dawlah@utdallas.edu>
2025-10-16 13:47:32 -0500

    made env variables public

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/YOUR-REPO-NAME/'  // Replace with your repo name
+  base: '/Cooking-by-Conversation/'  // Replace with your repo name
 })
